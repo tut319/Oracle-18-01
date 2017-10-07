cb585a3dbe0e4a9af86c16dfc6fc432d44f12fda
tut319 <32596582+tut319@users.noreply.github.com>
2017-10-07 09:50:59 -0500

    Add files via upload

--- Primer-Clase.sql
+++ Primer-Clase.sql
@@ -1,1 +1,5 @@
-Guardar: Primer clase.sql
+--creamos tabla almacen---
+create table almacen(numero_almacen integer,
+            ubicacion_almacen varchar2(80),
+        constraint pk_numero_almacen primary key(numero_almacen)
+        );
